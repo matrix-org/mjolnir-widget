eabd86b0396679d83e22a5ef1a6205163655b146
jesopo <github@lolnerd.net>
2022-09-07 13:57:10 +0000

    reformat webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    entry: "./index.ts",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "static/index.html"
-        }),
-        new HtmlInlineScriptPlugin()
+  entry: "./index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    resolve: {
-        extensions: [".css", ".js", ".ts"],
-    }
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "static/index.html",
+    }),
+    new HtmlInlineScriptPlugin(),
+  ],
+  resolve: {
+    extensions: [".css", ".js", ".ts"],
+  },
 };
